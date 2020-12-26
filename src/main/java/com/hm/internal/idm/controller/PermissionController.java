478c585bcebc7e0a61704848d61709c92383fda8
koti <thalari.koti@gmail.com>
2020-12-26 17:07:39 +0530

    added permission api

--- src/main/java/com/hm/internal/idm/controller/PermissionController.java
+++ src/main/java/com/hm/internal/idm/controller/PermissionController.java
@@ -1,17 +1,26 @@
 package com.hm.internal.idm.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hm.internal.idm.service.PermissionService;
 
 @RestController
 @RequestMapping(value="api")
 public class PermissionController {
 	
-	
+	@Autowired
+	PermissionService permissionService;
 
 	@GetMapping(value="/permission")
-	public String getAllPermissions() {
-		return "Welcome to permissions";
+	public ResponseEntity<Object>  getAllPermissions() {
+		ResponseEntity<Object> response=null;
+		 Object object= permissionService.getAllPermission();
+		 response=new ResponseEntity<Object>(object, HttpStatus.OK);
+		 return response;
 	}
 }
